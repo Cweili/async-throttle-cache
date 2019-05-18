6eaf2217a66c81048fadade07f7e2d95549b8ca8
Cweili <3weili@gmail.com>
2019-05-18 13:37:09 +0800

    docs: add comments to declaration file

--- index.d.ts
+++ index.d.ts
@@ -1,20 +1,43 @@
 declare namespace Options {
   declare interface key {
+    /**
+     * Specify how to generate cache key. Different cache key will re-invoke fn to get a new result.
+     */
     (...args: any[]): String
   }
 
   declare interface serialize {
+    /**
+     * Method for cached result, it could be asynchronous functions.
+     */
     (result: any): Promise<any>
   }
 
   declare interface deserialize {
+    /**
+     * Method for cached result, it could be asynchronous functions.
+     */
     (serialized: any): Promise<any>
   }
 }
 
+/**
+ * Creates a throttled function that only invokes `fn` at most once per every `wait` milliseconds, and returns cached result.
+ */
 declare function asyncThrottleCache(
+  /**
+   * Creates a throttled function that only invokes `fn` at most once per every `wait` milliseconds, and returns cached result.
+   */
   fn: Function,
+
+  /**
+   * Creates a throttled function that only invokes `fn` at most once per every `wait` milliseconds, and returns cached result.
+   */
   wait: Number,
+
+  /**
+   * 
+   */
   options: {
     key: Options.key
     serialize: Options.serialize
